ee72eba9675db213ad41f41f3ff9745469dcadc7
Elin Nyman <elin.nyman87@gmail.com>
2024-01-03 22:39:18 +0100

    setup userservice

---
+++ functions/services/user.service.js
@@ -0,0 +1,19 @@
+const db = require("../firebase.config");
+
+exports.createUser = async (name, profileURL, email) => {
+  try {
+    const newDocRef = await db.collection("users").add({
+      name,
+      profileURL,
+      email,
+    });
+
+    const docId = newDocRef.id;
+
+    await db.collection("users").doc(docId).update({ id: docId });
+
+    return { id: docId, name, profileURL, email };
+  } catch (error) {
+    throw error;
+  }
+};
