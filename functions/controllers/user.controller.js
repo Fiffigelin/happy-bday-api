4bdcbeaf9fc39dbfa8ffe9c6011520069bf8d866
Elin Nyman <elin.nyman87@gmail.com>
2024-01-03 22:42:39 +0100

    added controller and route to create user and implemented router to app

---
+++ functions/controllers/user.controller.js
@@ -0,0 +1,14 @@
+const userService = require("../services/userService");
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, profileURL, email } = req.body;
+    const createdUser = await userService.createUser(name, profileURL, email);
+    return res
+      .status(200)
+      .send({ status: "Success", msg: "Data Saved", user: createdUser });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ status: "Failed", msg: error });
+  }
+};
