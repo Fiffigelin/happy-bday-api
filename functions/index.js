d9348680cc198476412f8e68c554acf1b081bd3b
Elin Nyman <elin.nyman87@gmail.com>
2024-01-05 14:07:24 +0100

    Added image and tests

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,11 @@ const { app } = require("./firebase.config");
 const userRoutes = require("./routes/user.route");
 const contactRoutes = require("./routes/contact.route");
 const fileRoutes = require("./routes/file.route");
+const imageRoutes = require("./routes/image.route");
 
 app.use("/api/user", userRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/file", fileRoutes);
+app.use("/api/image", imageRoutes);
 
 exports.app = functions.https.onRequest(app);
