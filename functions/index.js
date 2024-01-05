5798963373d684cd307686e0ee391c71d74aa435
Elin Nyman <elin.nyman87@gmail.com>
2024-01-05 12:57:34 +0100

    clean up

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,7 @@
 // const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-// const express = require("express");
-// const cors = require("cors");
 const { app } = require("./firebase.config");
-// const { serviceAccount } = require("./firebase.config");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-// const app = express();
-// app.use(cors({ origin: true }));
 
 const userRoutes = require("./routes/user.route");
 const contactRoutes = require("./routes/contact.route");
