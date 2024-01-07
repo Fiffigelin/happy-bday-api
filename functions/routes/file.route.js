9cdfb4893a520239bb871df1b4385d8bab41b6af
Elin Nyman <elin.nyman87@gmail.com>
2024-01-07 15:30:44 +0100

    Start over

--- functions/routes/file.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const fileController = require("../controllers/file.controller");
-
-const router = express.Router();
-
-router.get("/get", fileController.getFiles);
-
-module.exports = router;
