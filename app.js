be1d066e844fa7d60f532a8fc3439c5810b651f4
Elin Nyman <elin.nyman87@gmail.com>
2024-02-18 14:28:51 +0100

    Configuring cron-settings

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { app } = require("./firebase.config");
 const config = require("./config");
+const cron = require("node-cron");
+const cronJob = require("./cron.job");
 
 const userRoutes = require("./src/routes/user.route");
 const contactRoutes = require("./src/routes/contact.route");
@@ -8,6 +10,10 @@ const imageRoutes = require("./src/routes/image.route");
 app.use("/api/user", userRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/image", imageRoutes);
+
+cron.schedule("* * * * *", () => {
+  cronJob.runCronJob();
+});
 
 app.listen(config.port, () =>
   console.log("App is listening on url " + config.url)
