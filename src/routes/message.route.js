a91b13f4c125373bab169a05552d750b44c52e94
Elin Nyman <elin.nyman87@gmail.com>
2024-04-05 18:05:43 +0200

    Connect message to contact

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -4,7 +4,8 @@ const messageController = require("../controllers/message.controller");
 const router = express.Router();
 
 router.post("/create", messageController.createMessage);
-router.get("/get/:id", messageController.getMessagesByUser);
+router.get("/get/user-msg/:id", messageController.getMessagesByUser);
+router.get("/get/contact-msg/:id", messageController.getMessageByContact);
 router.get("/get-msg/:id", messageController.getMessageById);
 
 module.exports = router;
